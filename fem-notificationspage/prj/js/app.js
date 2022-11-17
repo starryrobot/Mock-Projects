77ca90ed9e23413e4e97043e1f0a95edc4da0fb3
starryrobot <g_d@tuta.io>
2022-11-17 01:39:08 +0000

    JS code changes

--- fem-notificationspage/prj/js/app.js
+++ fem-notificationspage/prj/js/app.js
@@ -1,6 +1,8 @@
 const noteItem = document.querySelectorAll("article");
 const noteNumber = document.getElementById("notifs");
 let newNumber = 0;
+let currTarget = "";
+let array = [];
 
 console.log(`Array length is: ${noteItem.length}`);
 
@@ -8,24 +10,40 @@ document.addEventListener("DOMContentLoaded", function () {
   noteNumber.textContent = noteItem.length;
   newNumber = noteItem.length;
   for (i = 0; i < noteItem.length; i++) {
-    noteArray = noteItem[i];
-    console.log(`The array is: ${noteArray}`);
+    let array = new Array(noteItem[i][8]);
+    console.log(`This is the array: ${array}`);
     noteItem[i].addEventListener("click", function (e) {
+      console.log(this);
       e.currentTarget.classList.remove("new-item");
       e.currentTarget.classList.remove("new");
       e.currentTarget.classList.add("clicked");
+      if (this.classList.contains("clicked")) {
+        this.classList.remove("clicked");
+      }
     });
     console.log(noteItem[i]);
     if (noteItem[i].classList.contains("new")) {
       noteItem[i].classList.add("new-item");
     }
-    checkNotes;
   }
 });
 
-function checkNotes() {
-  console.log("checkNotes function called");
-  for (i = 0; i < noteItem.length; i++) {
-    noteItem[i].classList.remove("clicked");
-  }
-}
+// function checkNotes() {
+//   console.log("checkNotes function called");
+//   for (i = 0; i < noteItem.length; i++) {
+//     if (noteItem[i].classList.contains("clicked")) {
+//       console.log(array.length);
+//       array.pop();
+//       noteNumber.textContent = array.length;
+//     }
+//   }
+// }
+
+// function ()
+
+// function checkNotes() {
+//   console.log("checkNotes function called");
+//   for (i = 0; i < noteItem.length; i++) {
+//     noteItem[i].classList.remove("clicked");
+//   }
+// }
