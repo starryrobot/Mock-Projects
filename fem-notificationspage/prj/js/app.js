caa3b1dfdc01a3bae2184c94148365822a97cc1c
starryrobot <g_d@tuta.io>
2022-11-17 11:47:47 +0000

    JS code changes

--- fem-notificationspage/prj/js/app.js
+++ fem-notificationspage/prj/js/app.js
@@ -1,5 +1,6 @@
 const noteItem = document.querySelectorAll("article");
 const noteNumber = document.getElementById("notifs");
+const markAll = document.getElementById("mark");
 let newNumber = 0;
 let currTarget = "";
 let array = Array.from(noteItem);
@@ -8,38 +9,64 @@ let isNew = false;
 console.log(`Array length is: ${noteItem.length}`);
 
 document.addEventListener("DOMContentLoaded", noteCheck);
+markAll.addEventListener("click", markAllNotes);
 
 function noteCheck() {
-  console.log("noteCheck function called");
-  noteNumber.textContent = noteItem.length;
-  newNumber = noteItem.length;
   for (i = 0; i < noteItem.length; i++) {
-    console.log(`This is the array: ${array}`);
     noteItem[i].addEventListener("click", function (e) {
-      console.log(e.currentTarget.classList);
-      if (e.currentTarget.classList.contains("clicked")) {
-        console.log("Clicked");
-        e.currentTarget.setAttribute("notif-status", "old");
-        e.currentTarget.classList.remove("clicked");
-        e.currentTarget.setAttribute("disabled", "");
-        if (e.currentTarget.getAttribute("notif-status") === "old") {
-          e.currentTarget.removeAttribute("notif-status");
-          array.pop();
-          noteNumber.textContent = array.length;
-        }
-      } else {
-        e.currentTarget.classList.remove("new-item");
-        e.currentTarget.classList.remove("new");
-        e.currentTarget.classList.add("clicked");
+      if (e.currentTarget.classList.contains("unread")) {
+        console.log("Contains unread");
+        e.currentTarget.classList.remove("unread");
       }
+      noteCheck();
     });
-    console.log(noteItem[i]);
-    if (noteItem[i].classList.contains("new")) {
-      noteItem[i].classList.add("new-item");
-      noteItem[i].setAttribute("notif-status", "new");
-    }
+
+    noteNumber.textContent = document.querySelectorAll(".unread").length;
   }
 }
+
+function markAllNotes() {
+  for (i = 0; i < noteItem.length; i++) {
+    if (noteItem[i].classList.contains("unread")) {
+      noteItem[i].classList.remove("unread");
+    }
+    noteNumber.textContent = document.querySelectorAll(".unread").length;
+  }
+}
+
+// noteCheck();
+
+// function noteCheck() {
+//   console.log("noteCheck function called");
+//   noteNumber.textContent = noteItem.length;
+//   newNumber = noteItem.length;
+//   for (i = 0; i < noteItem.length; i++) {
+//     console.log(`This is the array: ${array}`);
+//     noteItem[i].addEventListener("click", function (e) {
+//       console.log(e.currentTarget.classList);
+//       if (e.currentTarget.classList.contains("clicked")) {
+//         console.log("Clicked");
+//         e.currentTarget.setAttribute("notif-status", "old");
+//         e.currentTarget.classList.remove("clicked");
+//         e.currentTarget.setAttribute("disabled", "");
+//         if (e.currentTarget.getAttribute("notif-status") === "old") {
+//           e.currentTarget.removeAttribute("notif-status");
+//           array.pop();
+//           noteNumber.textContent = array.length;
+//         }
+//       } else {
+//         e.currentTarget.classList.remove("new-item");
+//         e.currentTarget.classList.remove("new");
+//         e.currentTarget.classList.add("clicked");
+//       }
+//     });
+//     console.log(noteItem[i]);
+//     if (noteItem[i].classList.contains("new")) {
+//       noteItem[i].classList.add("new-item");
+//       noteItem[i].setAttribute("notif-status", "new");
+//     }
+//   }
+// }
 
 // function check() {
 //   for (i = 0; i < noteItem.length; i++) {
