5d78372406c7708564f482b09bfb07d1374ee819
starryrobot <g_d@tuta.io>
2022-11-24 23:35:17 +0000

    CSS code changes

---
+++ fem-expenseschart/prj/js/app.js
@@ -0,0 +1,28 @@
+// var ctx = document.getElementById("myChart");
+// var chart = new Chart(ctx, {
+//   // The type of chart we want to create
+//   type: "bar",
+
+//   // The data for our dataset
+//   data: {
+//     //Insert the divisions for the x-axis here
+//     labels: ["January", "February", "March", "April", "May", "June", "July"],
+//     datasets: [
+//       {
+//         label: "Temperature Data",
+//         backgroundColor: "rgb(255, 99, 132)",
+//         borderColor: "rgb(255, 99, 132)",
+//         //Insert data points here (y-axis)
+//         data: [40, 10, 5, 30, 20, 30, 50],
+//       },
+//     ],
+//   },
+
+//   // Configuration options go here
+//   options: {
+//     //causes chart to resize when its container resizes
+//     responsive: true,
+//     //setting to false will prevent the height of the chart from shrinking when resizing
+//     maintainAspectRatio: false,
+//   },
+// });
